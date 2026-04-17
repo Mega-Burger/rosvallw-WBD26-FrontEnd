e6cdf8f9ced886ab44d2d7a2df01a30496887b70
Mega-Burger <woldepo@gmail.com>
2026-04-17 14:28:23 +0300

    Hotel Frontend

--- hotel-frontend/scripts.js
+++ hotel-frontend/scripts.js
@@ -1,18 +1,49 @@
-console.log("hottels");
+console.log("hotels");
 
-//const apiUrl = 'https://rosvallw-csc-rahti-rosvallw-project-webbkommunikationer.2.rahtiapp.fi/api/ip';
 const apiUrl = "http://127.0.0.1:8080";
 
 async function getRooms() {
     const res = await fetch(`${apiUrl}/rooms`);
     const rooms = await res.json();
-    console.log("data");
-    for (room of rooms) {
+    console.log("rooms", rooms);
+    for (const room of rooms) {
         document.getElementById("room-list").innerHTML += `
-            <li>${room.room_number} - ${room.room_type} - ${room.price} €</li>
+            <option value="${room.id}">${room.room_number} - ${room.type} - ${room.price} €</option>
         `;
     }
-
-
 }
 getRooms();
+
+async function getBookings() {
+    const res = await fetch(`${apiUrl}/bookings`);
+    const bookings = await res.json();
+    console.log(bookings);
+    document.getElementById("bookings-list").innerHTML = "";
+    for (const booking of bookings) {
+        document.getElementById("bookings-list").innerHTML += `
+            <li>Room ${booking.room_number} — From: ${booking.datefrom} To: ${booking.dateto}</li>
+        `;
+    }
+}
+getBookings();
+
+async function saveBooking() {
+    const booking = {
+        room_id: document.getElementById("room-list").value,
+        guest_id: 1,
+        datefrom: document.getElementById("datefrom").value,
+        dateto: document.getElementById("dateto").value,
+        info: document.getElementById("info").value
+    };
+
+    const res = await fetch(`${apiUrl}/bookings`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(booking)
+    });
+    const resData = await res.json();
+    console.log(resData);
+    getBookings();
+}
+
+document.getElementById('btn-save').addEventListener('click', saveBooking);
