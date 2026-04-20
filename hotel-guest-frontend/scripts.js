65905159906af7e972dcdccf582771bc9088f0a4
Mega-Burger <woldepo@gmail.com>
2026-04-20 11:01:42 +0300

    --

---
+++ hotel-guest-frontend/scripts.js
@@ -0,0 +1,59 @@
+console.log("hotels");
+
+const apiUrl = "http://127.0.0.1:8080";
+
+const API_KEY = "1234567890"; // This should be stored securely in a real application and not hardcoded in the frontend
+
+/*async function getRooms() {
+    const res = await fetch(`${apiUrl}/rooms`);
+    const rooms = await res.json();
+    console.log("rooms", rooms);
+    for (const room of rooms) {
+        document.getElementById("room-list").innerHTML += `
+            <option value="${room.id}">${room.room_number} - ${room.type} - ${room.price} €</option>
+        `;
+    }
+}
+getRooms();*/
+
+async function getCurrentGuest() {
+    const res = await fetch(`${apiUrl}/guests/current`, {
+        headers: {'x-api-key': API_KEY}
+    });
+    const guest = await res.json();
+    console.log("guest", guest);
+}
+
+async function getBookings() {
+    const res = await fetch(`${apiUrl}/bookings`);
+    const bookings = await res.json();
+    console.log(bookings);
+    document.getElementById("bookings-list").innerHTML = "";
+    for (const booking of bookings) {
+        document.getElementById("bookings-list").innerHTML += `
+            <li>Room ${booking.room_number} — From: ${booking.datefrom} To: ${booking.dateto}</li>
+        `;
+    }
+}
+getBookings();
+
+async function saveBooking() {
+    const booking = {
+        room_id: document.getElementById("room-list").value,
+        guest_id: 1,
+        datefrom: document.getElementById("datefrom").value,
+        dateto: document.getElementById("dateto").value,
+        info: document.getElementById("info").value
+    };
+
+    const res = await fetch(`${apiUrl}/bookings`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(booking)
+    });
+    const resData = await res.json();
+    console.log(resData);
+    getBookings();
+}
+
+document.getElementById('btn-save').addEventListener('click', saveBooking);
